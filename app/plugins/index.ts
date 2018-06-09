9a5686aab83ef0ffd3aa370fadb833b54c796b58
SimonTesla <simon.tesla@thornvalley.com>
2018-06-09 01:31:50 -0700

    Added hentaifoundry support

--- app/plugins/index.ts
+++ app/plugins/index.ts
@@ -1,22 +1,10 @@
-//export * from './deviantart';
-//export * from './e621';
-//export * from './ekas';
-//export * from './furaffinity';
-//export * from './furrynetwork';
-//export * from './gfx'
-//export * from './hiccears';
-//export * from './inkbunny';
-//export * from './patreon';
-//export * from './shorpy';
-//export * from './sofurry';
-//export * from './weasyl';
-
 import './deviantart';
 import './e621';
 import './ekas';
 import './furaffinity';
 import './furrynetwork';
 import './gfx'
+import './hentaifoundry';
 import './hiccears';
 import './inkbunny';
 import './patreon';
