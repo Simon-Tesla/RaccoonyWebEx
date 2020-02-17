524207d96cfee7633919f5a7cd581861fdf369f0
Simon Tesla <simon.tesla@thornvalley.com>
2020-02-16 20:39:14 -0800

    weasyl fixes (untested)

--- app/plugins/weasyl.ts
+++ app/plugins/weasyl.ts
@@ -2,6 +2,7 @@ import * as I from '../definitions';
 import { default as BaseSitePlugin, registerPlugin } from './base';
 import { querySelectorAll, querySelector, getPageLinksFromAnchors } from '../utils/dom';
 import { getFilenameParts } from '../utils/file';
+import * as logger from '../logger';
 
 const serviceName = "weasyl";
 
@@ -18,10 +19,71 @@ export class WeasylPlugin extends BaseSitePlugin {
         }
         let button = buttonSpan.parentElement;
 
+        // Get the image URL from the download button
+        let url = button.getAttribute("href");
+
+        let { username, filename, id } = this.getMetadataFromCanonicalUrl();
+
+        // Grab the service filename from the download link.
+        let { serviceFilename, filename: downloadButtonFilename, ext } = this.getMetadataFromDownloadButtonLink(url);
+        // If filename is null, fall back to either the filename extracted from the download button or the service filename.
+        filename = filename || downloadButtonFilename || serviceFilename;
+
+        let previewImg: HTMLImageElement = querySelector('#detail-art img');
+        let previewUrl = (previewImg && previewImg.src) || null;
+
+        let titleElt = document.getElementById("detail-title")
+        let title = titleElt && titleElt.textContent.trim();
+        let descriptionElt = querySelectorAll("#detail-description .formatted-content").pop();
+        let description = descriptionElt && descriptionElt.textContent.trim();
+        let tags = querySelectorAll(".di-tags .tags a")
+            .map(el => el.textContent.trim());
+
+        let media: I.Media = {
+            url: url,
+            previewUrl: previewUrl,
+            author: username,
+            filename: filename,
+            siteFilename: serviceFilename,
+            extension: ext,
+            submissionId: id,
+            siteName: serviceName,
+            title: title,
+            description: description,
+            tags: tags
+        };
+        logger.log("weasyl: media ", media);
+        return Promise.resolve(media);
+    }
+
+    private getMetadataFromCanonicalUrl() {
+        // Get the "canonical" link from the page header, and take the href
+        // The canonical link is of the format
+        // https://www.weasyl.com/%7E[user]/submissions/[id]/[title]
+
+        let canonical = document.querySelector("link[rel=canonical]");
+        let canonicalLink = canonical.getAttribute("href");
+
+        // Get the username and kill leading '%7E'
+        let canonicalObj = new URL(canonicalLink);
+        let pathParts = canonicalObj.pathname.split('/'); // "/~user/..."
+        let username = pathParts[1] || "";
+        username = decodeURIComponent(username).replace(/^~/, "");
+
+        // Get the submission id
+        let id = pathParts[3] || "";
+        
+        // Use the title in the canonical link as part of the local filename
+        // Canonical link is silent on the extension (jpg, png, etc), so get that later
+        let filename = pathParts[4] || "";
+        
+        return { username, filename, id };
+    }
+
+    private getMetadataFromDownloadButtonLink(url: string) {
         // Get the URL
         // Weasyl download URLs are of the format 
         // https://cdn.weasyl.com/~[user]/submissions/[id]/[hash]/[user]-[filename.ext]?download
-        let url = button.getAttribute("href");
         let urlObj = new URL(url);
 
         // Get the username
@@ -38,30 +100,7 @@ export class WeasylPlugin extends BaseSitePlugin {
         let { filename, ext } = getFilenameParts(serviceFilename);
         filename = filename.substring(username.length + 1);
 
-        let previewImg: HTMLImageElement = querySelector('#detail-art img');
-        let previewUrl = (previewImg && previewImg.src) || null;
-
-        let titleElt = document.getElementById("detail-title")
-        let title = titleElt && titleElt.textContent.trim();
-        let descriptionElt = querySelectorAll("#detail-description .formatted-content").pop();
-        let description = descriptionElt && descriptionElt.textContent.trim();
-        let tags = querySelectorAll("#di-tags .tags a")
-            .map(el => el.textContent.trim());
-
-        let media: I.Media = {
-            url: url,
-            previewUrl: previewUrl,
-            author: username,
-            filename: filename,
-            siteFilename: serviceFilename,
-            extension: ext,
-            submissionId: id,
-            siteName: serviceName,
-            title: title,
-            description: description,
-            tags: tags
-        };
-        return Promise.resolve(media);
+        return { serviceFilename, filename, username, ext }
     }
 
     getPageLinkList(): Promise<I.PageLinkList> {
@@ -74,7 +113,7 @@ export class WeasylPlugin extends BaseSitePlugin {
             urlparts.pop();
             return urlparts.pop();
         });
-        
+
         let pageList: I.PageLinkList = {
             list: list,
             sortable: !nosort
