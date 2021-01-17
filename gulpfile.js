1b8794a5fce1126c25e2d4684897fc86087ae2fd
Simon Tesla <simon.tesla@thornvalley.com>
2021-01-16 22:12:33 -0800

    Fix build to produce non-minified output files

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,10 @@ gulp.task("typescript:compile", () => {
 function createBundleJsFile(filename) {
     return gulp.src([`${jsOutDir}/${filename}`])
         .pipe(webpack({
-            "output": { filename }
+            // Both "production" and "development" versions of webpack's output significantly modify the JS source. 
+            // The "none" output is most like the original JS.
+            "mode": "none",
+            "output": { filename },
         }))
         .pipe(gulp.dest(`${outputDir}/ext/`));
 }
