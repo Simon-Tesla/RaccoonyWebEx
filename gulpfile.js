cd9dfb9f699278bf46d0e1a5f96676c979ffa5de
Simon Tesla <simon.tesla@thornvalley.com>
2021-01-18 18:55:24 -0800

    Update updates.json for 1.2.4 and fix gulp for newer version of web-ext

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const del = require("del");
 const exists = require("path-exists").sync;
 const merge = require("gulp-merge-json");
 const ts = require("gulp-typescript");
-const webExt = require("web-ext").default;
+const webExt = require("web-ext");
 
 const sourceDir = 'app'; //TODO: move under src or something to be consistent
 const staticsDir = 'src'; //TODO: use statics or something
