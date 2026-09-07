ab868b8acfa34c86eb0a17fddd9e3145f9cf7b8c
Simon Tesla <simon.tesla@thornvalley.com>
2026-09-07 00:53:13 -0700

    Add page link support

--- app/plugins/mousepad.ts
+++ app/plugins/mousepad.ts
@@ -1,6 +1,6 @@
 import BaseSitePlugin, { registerPlugin } from "./base";
-import { Media } from "../definitions";
-import { querySelector, querySelectorAll } from "../utils/dom";
+import { Media, PageLinkList } from "../definitions";
+import { querySelector, querySelectorAll, getPageLinksFromSelector } from "../utils/dom";
 import { getFilenameParts } from "../utils/file";
 
 const serviceName = 'mousepad'
@@ -12,7 +12,9 @@ export class MousepadPlugin extends BaseSitePlugin {
 
   async getMedia(): Promise<Media> {
     // Mousepad is so easy to parse, this code is basically self-documenting.
+    // Seriously, the only way this would be more straightforward is if Kammy just stuck a JSON blob in the exact format I needed on the page.
     const downloadLink = querySelector<HTMLAnchorElement>('#download-link');    
+    if (!downloadLink) { return }
     const url = downloadLink.href;
     
     const imgLink = querySelector<HTMLImageElement>('#main-submission-image');
@@ -47,6 +49,26 @@ export class MousepadPlugin extends BaseSitePlugin {
       title,
       description,
       tags
+    }
+  }
+
+  async getPageLinkList(): Promise<PageLinkList> {
+    if (window.location.pathname.startsWith('/new-uploads')) {
+      // The new uploads page has a slightly different gallery format.
+      const list = getPageLinksFromSelector('.notifications-content-short-form .title a', (href) => href.split('/').pop())
+      return {
+        sortable: true,
+        list
+      }
+    }
+
+    // Everything else pretty much looks like this. This doesn't get links from profile pages or the main page, 
+    // but works everywhere else (e.g. galleries, pools, search, favorites).
+    const list = getPageLinksFromSelector('#gallery-content-container .title a.formattable-gallery-link');
+
+    return {
+      sortable: false,
+      list 
     }
   }
 }
